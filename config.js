ba4ea3119e43f5e8042f93148ac7e65881020162
ALBERT-tech <63470407+ALBERT-tech@users.noreply.github.com>
2026-02-01 16:24:07 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,11 @@
 // Заполни значения из Supabase Project Settings → API.
 window.APP_CONFIG = {
   SUPABASE_URL: "https://nbdhypvqoeytigopqkwr.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_YYEE9vUf479k1dXGaIfKvg_sM8kvtc5",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5iZGh5cHZxb2V5dGlnb3Bxa3dyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5MzU1NTgsImV4cCI6MjA4NTUxMTU1OH0.FieVAdJfaw5OSd-9Ym0Bky2xx2M6fienNUyS4JvKcLg",
   TABLE_NAME: "responses",
   // optional: lock to expected questionnaire_id
   EXPECTED_QUESTIONNAIRE_ID: "patient_form"
 };
+
 
 
