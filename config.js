98a8b90c7a351f5c030917deee685154491809fc
ALBERT-tech <63470407+ALBERT-tech@users.noreply.github.com>
2026-02-01 15:35:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,9 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://nbdhypvqoeytigopqkwr.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_YYEE9vUf479k1dXGaIfKvg_sM8kvtc5",
-  TABLE_NAME: "public.responses",
+  TABLE_NAME: "responses",
   // optional: lock to expected questionnaire_id
   EXPECTED_QUESTIONNAIRE_ID: "patient_form"
 };
+
 
